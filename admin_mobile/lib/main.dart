42d8f2d84f18dba3b77933be36f2f55677fbf6a4
tadakun8 <tadakun8.lab@gmail.com>
2021-10-08 22:01:32 +0900

    feat: create nabigation bar

--- admin_mobile/lib/main.dart
+++ admin_mobile/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home: AdminMobilePage(),
+      home: const AdminMobilePage(),
     );
   }
 }
@@ -27,8 +28,46 @@ class AdminMobilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Row(
-        children: const <Widget>[Text('data')],
+        children: const <Widget>[SideNavigation()],
       ),
+    );
+  }
+}
+
+// ナビゲーションバーが選択している項目のインデックスを保持する
+final selectedIndexProivider = StateProvider((_) => 0);
+
+class SideNavigation extends ConsumerWidget {
+  const SideNavigation({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final selectedIndex = watch(selectedIndexProivider).state;
+    return NavigationRail(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: (index) {
+        context.read(selectedIndexProivider).state = index;
+      },
+      // 選択するとラベルが表示されるが、デフォルトだとラベルの文字列長だけ
+      // ナビゲーションバーの幅が広がる
+      // labelType: NavigationRailLabelType.selected,
+      destinations: const [
+        NavigationRailDestination(
+          icon: Icon(Icons.thumbs_up_down),
+          label: Text('ThumbsUpDown'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.people),
+          label: Text('People'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.face),
+          label: Text('Face'),
+        ),
+        NavigationRailDestination(
+          icon: Icon(Icons.bookmark),
+          label: Text('Bookmark'),
+        ),
+      ],
     );
   }
 }

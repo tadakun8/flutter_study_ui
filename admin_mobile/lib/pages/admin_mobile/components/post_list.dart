caafb7b8eac473cb0744e88f5d376e5733bda90c
tadakun8 <tadakun8.lab@gmail.com>
2021-10-09 14:14:41 +0900

    feat: show posts

--- admin_mobile/lib/pages/admin_mobile/components/post_list.dart
+++ admin_mobile/lib/pages/admin_mobile/components/post_list.dart
@@ -1,3 +1,4 @@
+import 'package:admin_mobile/pages/admin_mobile/components/post.dart';
 import 'package:admin_mobile/pages/admin_mobile/components/post_header.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,20 @@ class PostList extends StatelessWidget {
       child: Column(
         // 親Containerの高さを最大まで広げる
         mainAxisSize: MainAxisSize.max,
-        children: const [
-          PostsHeader(),
+        children: [
+          const PostsHeader(),
+          Expanded(
+            child: (ListView(
+              children: const [
+                PostGreen(),
+                PostRed(),
+                PostGreen(),
+                PostRed(),
+                PostGreen(),
+                PostRed(),
+              ],
+            )),
+          ),
         ],
       ),
     );

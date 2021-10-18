26cb147a185802bf4f0d33a26411412701e4b87e
tadakun8 <tadakun8.lab@gmail.com>
2021-10-18 21:49:17 +0900

    feat: show ticket

--- flight_booking/lib/const.dart
+++ flight_booking/lib/const.dart
@@ -5,3 +5,6 @@ const kColorFlightText = Color(0xFFE0E0E0);
 const kColorFlightIcon = Color(0xFFC1B695);
 const kColorText = Color(0xFF9E9E9E);
 const kColorTextDark = Color(0xFF212121);
+const kColorTicketBorder = Color(0xFFE0E0E0);
+const kSingaporeLogoUrl =
+    'https://user-images.githubusercontent.com/7200238/82220821-1ebc8880-995a-11ea-9d77-07edda64f05c.png';
--- flight_booking/lib/page/components/ticket_list.dart
+++ flight_booking/lib/page/components/ticket_list.dart
@@ -1,5 +1,264 @@
+import 'dart:ui';
+
 import 'package:flight_booking/const.dart';
 import 'package:flutter/material.dart';
+
+class _TicketShapeBorder extends ShapeBorder {
+  final double? width;
+  final double? radius;
+
+  const _TicketShapeBorder({
+    required this.width,
+    required this.radius,
+  });
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+    return EdgeInsets.all(width!);
+  }
+
+  // scaleもお作法
+  @override
+  ShapeBorder scale(double t) {
+    return _TicketShapeBorder(
+      width: width! * t,
+      radius: radius! * t,
+    );
+  }
+
+  // lerpFromの中はお作法
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is _TicketShapeBorder) {
+      return _TicketShapeBorder(
+        width: lerpDouble(a.width, width, t),
+        radius: lerpDouble(a.radius, radius, t),
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  // lerpToの中はお作法
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is _TicketShapeBorder) {
+      return _TicketShapeBorder(
+        width: lerpDouble(width, b.width, t),
+        radius: lerpDouble(radius, b.radius, t),
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return getOuterPath(
+      // deflate(指定した幅だけ縮小する)
+      rect.deflate(width!),
+      textDirection: textDirection,
+    );
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    final r = radius!;
+    final rs = radius! / 2; // 区切り部分の半径
+    final w = rect.size.width; // 全体の横幅
+    final h = rect.size.height; // 全体の縦幅
+    final wl = w / 3; // ロゴ部分の横幅
+    return Path()
+      ..addPath(
+        Path()
+          // 左上の曲がった後からスタート
+          ..moveTo(r, 0)
+          ..lineTo(wl - rs, 0) // →
+          ..arcToPoint(
+            Offset(wl + rs, 0),
+            radius: Radius.circular(rs),
+            // 反時計回りに描く
+            // この場合は下に半円を描く
+            clockwise: false,
+          )
+          // 曲がる前の右上まで
+          ..lineTo(w - r, 0) // →
+          // 右から下に円を描きながら曲がる
+          ..arcToPoint(Offset(w, r), radius: Radius.circular(r))
+          ..lineTo(w, h - rs) // ↓
+          ..arcToPoint(Offset(w - r, h), radius: Radius.circular(r))
+          ..lineTo(wl + rs, h) // ←
+          ..arcToPoint(
+            Offset(wl - rs, h),
+            radius: Radius.circular(rs),
+            clockwise: false,
+          )
+          ..lineTo(r, h) // ←
+          ..arcToPoint(Offset(0, h - r), radius: Radius.circular(r))
+          ..lineTo(0, r) // ↑
+          ..arcToPoint(Offset(r, 0), radius: Radius.circular(r)),
+
+        // どこからplotするかの座標
+        // 左上からスタート
+        Offset(rect.left, rect.top),
+      );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    final paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = width!
+      ..color = kColorTicketBorder;
+    canvas.drawPath(
+      getOuterPath(
+        rect.deflate(width! / 2.0),
+        textDirection: textDirection,
+      ),
+      paint,
+    );
+  }
+}
+
+class _Ticket extends StatelessWidget {
+  final Widget image;
+  const _Ticket({
+    Key? key,
+    required this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 128,
+      decoration: const ShapeDecoration(
+        color: Colors.white,
+        shape: _TicketShapeBorder(width: 1, radius: 16.0),
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              child: image,
+            ),
+          ),
+          Container(
+            width: 1,
+            height: double.infinity,
+            margin: const EdgeInsets.symmetric(
+              vertical: 8.0,
+            ),
+            color: kColorTicketBorder,
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Table(
+                children: [
+                  TableRow(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Depature',
+                            style: TextStyle(color: kColorText),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '04:25 pm',
+                            style: TextStyle(
+                              color: kColorTextDark,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Arrive',
+                            style: TextStyle(color: kColorText),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '07:55 pm',
+                            style: TextStyle(
+                              color: kColorTextDark,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const TableRow(
+                    children: [
+                      SizedBox(height: 8),
+                      SizedBox(height: 8),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            'Estimation',
+                            style: TextStyle(color: kColorText),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '4h, 30m',
+                            style: TextStyle(
+                              color: kColorTextDark,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            '\$250.00',
+                            style: TextStyle(
+                              color: kColorPrimary,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            '/person',
+                            style: TextStyle(
+                              color: kColorText,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 class TicketList extends StatelessWidget {
   const TicketList({Key? key}) : super(key: key);
@@ -38,9 +297,11 @@ class TicketList extends StatelessWidget {
                   vertical: 8,
                 ),
                 child: Column(
-                  children: const [
-                    Text('sample'),
-                    SizedBox(height: 8),
+                  children: [
+                    _Ticket(
+                      image: Image.network(kSingaporeLogoUrl),
+                    ),
+                    const SizedBox(height: 8),
                   ],
                 ),
               ),

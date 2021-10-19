d9aebb188f9e27903733656f2ee56c1711b51a4d
tadakun8 <tadakun8.lab@gmail.com>
2021-10-19 16:35:18 +0900

    feat: show hero on top page

--- online_learning/lib/components/top_page.dart
+++ online_learning/lib/components/top_page.dart
@@ -24,6 +24,52 @@ class _Header extends StatelessWidget {
   }
 }
 
+class _Hero extends StatelessWidget {
+  const _Hero({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        color: Colors.blue[50],
+        child: Container(
+          width: double.infinity,
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                width: 150,
+                child: Text('Choose from over 100,000 online video courses'),
+              ),
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () {},
+                child: const Text('Browse all courses'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 32,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
 
@@ -39,7 +85,7 @@ class TopPage extends StatelessWidget {
                 _Header(
                   title: 'TurtleU',
                 ),
-                // _Hero(),
+                _Hero(),
                 // _Featured(),
                 // _TrendingCourses(),
               ],

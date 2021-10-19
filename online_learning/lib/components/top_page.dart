de4140f62275f254e986d7a4b076285900841cf4
tadakun8 <tadakun8.lab@gmail.com>
2021-10-19 16:17:00 +0900

    feat: show header on top page

--- online_learning/lib/components/top_page.dart
+++ online_learning/lib/components/top_page.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
+class _Header extends StatelessWidget {
+  final String title;
+  const _Header({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 24),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
 
@@ -12,8 +36,9 @@ class TopPage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: const [
-                Text('sample')
-                // _Header(),
+                _Header(
+                  title: 'TurtleU',
+                ),
                 // _Hero(),
                 // _Featured(),
                 // _TrendingCourses(),

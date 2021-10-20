70aae783928c009c417ab12a8a51e28b5eaac176
tadakun8 <tadakun8.lab@gmail.com>
2021-10-20 19:07:40 +0900

    feat: show feature info

--- online_learning/lib/components/top_page.dart
+++ online_learning/lib/components/top_page.dart
@@ -70,6 +70,72 @@ class _Hero extends StatelessWidget {
   }
 }
 
+class _Featured extends StatelessWidget {
+  const _Featured({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          padding: const EdgeInsets.only(top: 32, left: 8),
+          alignment: Alignment.centerLeft,
+          child: const Text(
+            'Featured',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 24),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8,
+                    vertical: 32,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Icon(Icons.navigate_before),
+                      SizedBox(
+                        width: 180,
+                        child: Column(
+                          children: const [
+                            Text(
+                              'Figma: Solid Foundations',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            Text(
+                              'The most complete beginner to advanced guide',
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Icon(Icons.navigate_next),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
 
@@ -86,7 +152,7 @@ class TopPage extends StatelessWidget {
                   title: 'TurtleU',
                 ),
                 _Hero(),
-                // _Featured(),
+                _Featured(),
                 // _TrendingCourses(),
               ],
             ),

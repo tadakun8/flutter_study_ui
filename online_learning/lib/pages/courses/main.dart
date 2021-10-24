49b271b0d53cebcaa9a7ef39e3c55bb2179af7c5
tadakun8 <tadakun8.lab@gmail.com>
2021-10-24 21:19:14 +0900

    feat: display animation at page transition

--- online_learning/lib/pages/courses/main.dart
+++ online_learning/lib/pages/courses/main.dart
@@ -2,8 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:online_learning/pages/courses/recommended_courses.dart';
 import 'package:online_learning/shared/componensts/header.dart';
 
-class CoursesPage extends StatelessWidget {
+class CoursesPage extends StatefulWidget {
   const CoursesPage({Key? key}) : super(key: key);
+
+  @override
+  _CoursesPageState createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animationHorizontal;
+  late Animation<Offset> _animationVertical;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    _animationHorizontal = Tween<Offset>(
+      begin: const Offset(-1.0, 0),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutBack,
+      ),
+    );
+    _animationVertical = Tween<Offset>(
+      begin: const Offset(0.0, 1.5),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutBack,
+      ),
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,9 +60,15 @@ class CoursesPage extends StatelessWidget {
             child: Container(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Column(
-                children: const [
-                  Header(title: 'Courses'),
-                  Recommended(),
+                children: [
+                  SlideTransition(
+                    position: _animationHorizontal,
+                    child: const Header(title: 'Courses'),
+                  ),
+                  SlideTransition(
+                    position: _animationVertical,
+                    child: const Recommended(),
+                  ),
                 ],
               ),
             ),
@@ -26,7 +77,7 @@ class CoursesPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).pop();
+          _controller.reverse().then((_) => Navigator.of(context).pop());
         },
         child: const Icon(Icons.keyboard_backspace),
       ),
--- online_learning/lib/pages/top/main.dart
+++ online_learning/lib/pages/top/main.dart
@@ -7,8 +7,63 @@ import 'components/featured.dart';
 import '../../shared/componensts/header.dart';
 import 'components/info.dart';
 
-class TopPage extends StatelessWidget {
+class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
+
+  @override
+  _TopPageState createState() => _TopPageState();
+}
+
+class _TopPageState extends State<TopPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation1;
+  late Animation<Offset> _animation2;
+  late Animation<Offset> _animation3;
+  late Animation<Offset> _animation4;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    // アニメーションの座標範囲
+    final tween = Tween<Offset>(
+      // 現在地点から
+      begin: Offset.zero,
+      // 左に遷移(-1.0なので画面から表示されなくなる)
+      end: const Offset(-1.0, 0.0),
+    );
+
+    // CurvedAnimationで弾んだようなアニメーションを作成する
+    // 軌跡はCurves.XXXによる
+    // tween.animate()でこの座標範囲の間でどのようなアニメーションをするか
+    _animation1 = tween.animate(CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.0, 0.7, curve: Curves.easeInOutBack),
+    ));
+    _animation2 = tween.animate(CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.1, 0.8, curve: Curves.easeInOutBack),
+    ));
+    _animation3 = tween.animate(CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.2, 0.9, curve: Curves.easeInOutBack),
+    ));
+    _animation4 = tween.animate(CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.3, 1.0, curve: Curves.easeInOutBack),
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,13 +73,25 @@ class TopPage extends StatelessWidget {
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
-              children: const [
-                Header(
-                  title: 'TurtleU',
+              children: [
+                SlideTransition(
+                  position: _animation1,
+                  child: const Header(
+                    title: 'TurtleU',
+                  ),
                 ),
-                Info(),
-                Featured(),
-                TrendingCources(),
+                SlideTransition(
+                  position: _animation2,
+                  child: const Info(),
+                ),
+                SlideTransition(
+                  position: _animation3,
+                  child: const Featured(),
+                ),
+                SlideTransition(
+                  position: _animation4,
+                  child: const TrendingCources(),
+                ),
               ],
             ),
           ),
@@ -32,12 +99,16 @@ class TopPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
-            // アニメーションを伴う遷移の時はMaterialPageRoute()ではなくPageRouteBuildder()
-            PageRouteBuilder(
-              pageBuilder: (_, __, ___) => const CoursesPage(),
-            ),
-          );
+          // アニメーション再生し、ページ遷移
+          _controller.forward().then((_) => Navigator.of(context)
+              .push(
+                // アニメーションを伴う遷移の時はMaterialPageRoute()ではなくPageRouteBuildder()
+                PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => const CoursesPage(),
+                ),
+              )
+              // 戻ってくるときは逆再生
+              .then((_) => _controller.reverse()));
         },
         child: const Icon(Icons.list),
       ),

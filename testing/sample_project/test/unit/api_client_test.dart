4df4e1e726a3e130e810292730cddc6d43679f3e
tadakun8 <tadakun8.lab@gmail.com>
2021-11-04 18:06:07 +0900

    test: add test case of ApiClient

--- testing/sample_project/test/unit/api_client_test.dart
+++ testing/sample_project/test/unit/api_client_test.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mock_web_server/mock_web_server.dart';
+import 'package:sample_project/models/album.dart';
 import 'package:sample_project/shared/api_client.dart';
 
 void main() {
@@ -26,6 +29,49 @@ void main() {
     test('HTTP Status is not 200', () async {
       _server.enqueue(httpCode: 500);
       expect(httpClient.get('sample'), throwsException);
+    });
+  });
+
+  group('ApiClient', () {
+    final apiCliet = ApiClient('http://localhost:8045/');
+    test('getAlbum', () async {
+      final mockResponseTemplate = {
+        "userId": 1,
+        "id": 1,
+        "title": "quidem molestiae enim"
+      };
+
+      final mockResponseBody = json.encode(mockResponseTemplate);
+
+      final mockResponse = MockResponse()
+        ..httpCode = 200
+        ..body = mockResponseBody;
+
+      _server.enqueueResponse(mockResponse);
+
+      final response = await apiCliet.getAlbum(1);
+      expect(response, Album.fromJson(mockResponseTemplate));
+    });
+
+    test('getAlbumList', () async {
+      final mockResponseTemplate = [
+        {"userId": 1, "id": 1, "title": "quidem molestiae enim"},
+        {"userId": 1, "id": 2, "title": "sunt qui excepturi placeat culpa"},
+        {"userId": 1, "id": 3, "title": "omnis laborum odio"},
+      ];
+
+      final mockResponseBody = json.encode(mockResponseTemplate);
+
+      final mockResponse = MockResponse()
+        ..httpCode = 200
+        ..body = mockResponseBody;
+
+      _server.enqueueResponse(mockResponse);
+
+      final response = await apiCliet.getAlbumList();
+      final expectedValue =
+          mockResponseTemplate.map((album) => Album.fromJson(album)).toList();
+      expect(response, expectedValue);
     });
   });
 }

cf3a71a3ecd46ffc00db68bfb19e9339c3562663
tadakun8 <tadakun8.lab@gmail.com>
2021-12-14 18:11:02 +0900

    feat: create template of sample page with flutter hook

---
+++ ui/scroll_controller/lib/pages/sample_page_with_flutter_hooks.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final isShowNotifierStateProvider = StateProvider((ref) => false);
+
+class SamplePageWithFlutterHook extends HookConsumerWidget {
+  const SamplePageWithFlutterHook({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final _scrollController = useScrollController();
+    final isShowNotifier = ref.watch(isShowNotifierStateProvider);
+
+    // スクロールを検知したときに呼ばれる処理
+    void _scrollListener() {
+      const threshold = 0.8;
+      double positionRate =
+          _scrollController.offset / _scrollController.position.maxScrollExtent;
+      if (positionRate > threshold) {
+        ref.read(isShowNotifierStateProvider.state).state = true;
+      }
+      if (positionRate < threshold - 0.05) {
+        ref.read(isShowNotifierStateProvider.state).state = false;
+      }
+    }
+
+    return Container();
+  }
+}

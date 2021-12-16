d93a1248f3c700b3c7071d7aa91dc4a5b1cd8810
tadakun8 <tadakun8.lab@gmail.com>
2021-12-16 19:34:42 +0900

    feat: display sampla page with full function

--- ui/scroll_controller/lib/pages/sample_page_with_flutter_hooks.dart
+++ ui/scroll_controller/lib/pages/sample_page_with_flutter_hooks.dart
@@ -11,6 +11,32 @@ class SamplePageWithFlutterHook extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final _scrollController = useScrollController();
     final isShowNotifier = ref.watch(isShowNotifierStateProvider);
+    int _currentIndex = 0;
+    final double itemHeight = 80.0;
+    var servantList = [
+      "Savar",
+      "Archer",
+      "Lancer",
+      "Rider",
+      "Caster",
+      "Assassin",
+      "Berserker",
+      "Ruler",
+      "Avenger",
+      "Alterego",
+      "Mooncancer",
+      "Savar",
+      "Archer",
+      "Lancer",
+      "Rider",
+      "Caster",
+      "Assassin",
+      "Berserker",
+      "Ruler",
+      "Avenger",
+      "Alterego",
+      "Mooncancer"
+    ];
 
     // スクロールを検知したときに呼ばれる処理
     void _scrollListener() {
@@ -19,14 +45,75 @@ class SamplePageWithFlutterHook extends HookConsumerWidget {
           _scrollController.offset / _scrollController.position.maxScrollExtent;
       if (positionRate > threshold) {
         ref.read(isShowNotifierStateProvider.state).state = true;
-      }
-      if (positionRate < threshold - 0.05) {
+      } else {
         ref.read(isShowNotifierStateProvider.state).state = false;
       }
     }
 
-    return Container(
-      child: Text("sample"),
+    _scrollController.addListener(_scrollListener);
+
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+        child: Stack(
+          children: [
+            if (ref.read(isShowNotifierStateProvider))
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                width: double.infinity,
+                color: Colors.orange[100],
+                child: const Text(
+                  'そろそろスクロールが終わるよ',
+                  style: TextStyle(color: Colors.red),
+                ),
+              ),
+            ListView.builder(
+              controller: _scrollController,
+              itemCount: 22,
+              itemBuilder: (BuildContext context, int position) {
+                return SizedBox(
+                  height: itemHeight,
+                  child: ListTile(
+                    title: Text(servantList[position]),
+                    subtitle: const Text('これはサンプルです'),
+                    leading: const Icon(Icons.light),
+                    trailing: const Icon(Icons.menu),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.keyboard_arrow_up),
+            label: 'UP',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.keyboard_arrow_down),
+            label: 'DOWN',
+          ),
+        ],
+        onTap: (int index) {
+          if (index == 0) {
+            _scrollController.animateTo(
+              _scrollController.offset - itemHeight,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          }
+          if (index == 1) {
+            _scrollController.animateTo(
+              _scrollController.offset + itemHeight,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+            );
+          }
+        },
+      ),
     );
   }
 }
